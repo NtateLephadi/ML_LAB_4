ebc1441b63a4749fbf40e1f1ac68740e73beb18d
Student <Student@SCI000A056.wf.uct.ac.za>
2018-11-02 15:58:59 +0200

    header

--- perceptron.h
+++ perceptron.h
@@ -1,2 +1,16 @@
 #include <vector>
 #include <iostream>
+
+#ifndef PERCEPTRON_H
+#define PERCEPTRON_H
+class perceptron{
+private:
+	double new_weight, old_weight, threshold;
+public:
+	perceptron();
+	~perceptron();
+	void to_string();
+	void set_new_weight();
+	void set_old_weight();
+};
+#endif
