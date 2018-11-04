0bb13f74febdda7a57d7c29104b89bac1005630c
Tumelo Lephadi <lphtum003@sl-dual-182.cs.uct.ac.za>
2018-11-04 21:58:09 +0200

    changed prototype

--- perceptron.h
+++ perceptron.h
@@ -11,7 +11,7 @@ private:
 	std::vector<std::vector<double>> inputs;
 	std::vector<double> weights
 	double learning_rate;
-	std::vector<int> output, target;
+	int output, target;
 public:
 	perceptron();
 	~perceptron();
@@ -19,8 +19,8 @@ public:
 	void set_weights(<std::vector<double>);
 	void set_inputs(std::vector<std::vector<double>>);
 	void set_learning_rate(double);
-	void set_output(std::vector<int>);
-	void set_target(std::vector<int>);
+	void set_output(int);
+	void set_target(int);
 	double product_sum();
 	void activation_function();
 	void percepton_rule();
