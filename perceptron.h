875aa5daa964263cccd2594b23c27f6f7782b01b
Tumelo Lephadi <lphtum003@sl-dual-182.cs.uct.ac.za>
2018-11-04 21:31:46 +0200

    changed weights

--- perceptron.h
+++ perceptron.h
@@ -8,19 +8,20 @@
 #define PERCEPTRON_H
 class perceptron{
 private:
-	std::vector<std::vector<double>> weights, inputs;
+	std::vector<std::vector<double>> inputs;
+	std::vector<double> weights
 	double learning_rate;
 	std::vector<int> output, target;
 public:
 	perceptron();
 	~perceptron();
 	void to_string();
-	void set_weights(std::vector<std::vector<double>>);
+	void set_weights(<std::vector<double>);
 	void set_inputs(std::vector<std::vector<double>>);
 	void set_learning_rate(double);
 	void set_output(std::vector<int>);
 	void set_target(std::vector<int>);
-	std::vector<double> product_sum();
+	double product_sum();
 	void activation_function();
 	void percepton_rule();
 	bool find_error();
