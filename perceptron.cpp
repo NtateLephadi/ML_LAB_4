e02fb490a369fac2508cb4262a9a3b74d420aaae
Tumelo Lephadi <lphtum003@sl-dual-182.cs.uct.ac.za>
2018-11-04 21:56:35 +0200

    changed perceptron.cpp

--- perceptron.cpp
+++ perceptron.cpp
@@ -4,25 +4,19 @@ perceptron::perceptron(){
 	unsigned seed = std::chrono::system_clock::now().time_since_epoch().count();
 	std::default_random_engine generator (seed);
 	std::uniform_real_distribution<double> distribution (-1.0,1.0);
-	std::vector<double> v;
-	for (size_t i = 0; i < 4; i++) {
+	for (size_t j = 0; j < 2; j++) {
 		/* code */
-		for (size_t j = 0; j < 2; j++) {
-			/* code */
-			v.push_back(distribution(generator));
-		}
-		this->weights.push_back(v);
-		v.clear();
+		this->weights.push_back(distribution(generator));
 	}
 }
 
 perceptron::~perceptron(){}
 
-void perceptron::set_weights(std::vector<std::vector<double>> v){
+void perceptron::set_weights(std::vector<double> v){
 	this->weights = v;
 }
 
-void perceptron::set_inputs(std::vector<std::vector<double>> v){
+void perceptron::set_inputs(std::vector<std::vector<int>> v){
   this->inputs = v;
 }
 
@@ -30,80 +24,62 @@ void perceptron::set_learning_rate(double d){
   this->learning_rate = d;
 }
 
-void perceptron::set_output(std::vector<int> i){
+void perceptron::set_output(int i){
 	this->output = i;
 }
 
-void perceptron::set_target(std::vector<int> t) {
+void perceptron::set_target(int i) {
 	/* code */
-	this->target=t;
+	this->target=i;
 }
 
-std::vector<double> perceptron::product_sum(){
-	std::vector<double> v;
+double perceptron::product_sum(){
 	double product_sum;
 	for (size_t j = 0; j < this->weights.size(); j++) {
 		/* code */
-		product_sum=0;
-		for (size_t i = 0; i < this->weights[j].size(); i++) {
-			/* code */
-			product_sum+=this->weights[j][i]*this->inputs[j][i];
-		}
-		v.push_back(product_sum);
+		product_sum+=this->weights[j]*this->inputs[j];
 	}
-	return v;
+	return product_sum;
 }
 
 void perceptron::activation_function(){
-	this->output.clear();
-	for (size_t i = 0; i < this->product_sum().size(); i++) {
-		/* code */
-		if(this->product_sum()[i]>0){
-			this->output.push_back(1);
-		}
-		else{
-			this->output.push_back(0);
-		}
+	/* code */
+	if(this->product_sum()>0){
+		this->output = 1;
+	}
+	else{
+		this->output = 0;
 	}
 }
 
 void perceptron::percepton_rule(){
 	this->activation_function();
-	for (size_t j = 0; j < this->weights.size(); j++) {
+	for (size_t i = 0; i < this->weights.size(); i++) {
 		/* code */
-		for (size_t i = 0; i < this->weights[j].size(); i++) {
-			/* code */
-			this->weights[j][i]=this->weights[j][i]+(
-				this->learning_rate*(
-					(
-						this->target[i]-this->output[i]
-					)*this->inputs[j][i]
-				)
-			);
-		}
+		this->weights[i]=this->weights[i]+(
+			this->learning_rate*(
+				(
+				this->target-this->output
+				)*this->inputs[i]
+			)
+		);
 	}
 }
 
 bool perceptron::find_error(){
 	bool error = false;
-	for (size_t i = 0; i < this->target.size(); i++) {
+	if (this->target!=this->output) {
 		/* code */
-		if (this->target[i]!=this->output[i]) {
-			/* code */
-			error = true;
-		}
+		error = true;
 	}
 	return error;
 }
 
 void perceptron::to_string(){
 	std::cout << "weights" << '\n';
-	for (size_t i = 0; i < this->weights.size(); i++) {
+	for (size_t j = 0; j < this->weights.size(); j++) {
 		/* code */
-		for (size_t j = 0; j < this->weights[i].size(); j++) {
-			/* code */
-			std::cout << this->weights[i][j] << '\n';
-		}
+		std::cout << this->weights[j] << '\n';
 	}
 
 	std::cout << "inputs" << '\n';
@@ -119,14 +95,10 @@ void perceptron::to_string(){
 	std::cout << this->learning_rate << '\n';
 
 	std::cout << "output" << '\n';
-	for (size_t i = 0; i < this->output.size(); i++) {
-		/* code */
-		std::cout << this->output[i] << '\n';
-	}
+	/* code */
+	std::cout << this->output << '\n';
 
 	std::cout << "target" << '\n';
-	for (size_t i = 0; i < this->target.size(); i++) {
-		/* code */
-		std::cout << this->target[i] << '\n';
-	}
+	/* code */
+	std::cout << this->target << '\n';
 }
